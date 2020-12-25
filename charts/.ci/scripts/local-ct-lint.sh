1ccc94bde1fbad84cc85531f802e33b63f3b0a6d
David Young <davidy@funkypenguin.co.nz>
2020-12-26 10:21:13 +1300

    Update for main branch (not master)

--- charts/.ci/scripts/local-ct-lint.sh
+++ charts/.ci/scripts/local-ct-lint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker run --rm -it -w /repo -v `pwd`:/repo quay.io/helmpack/chart-testing ct lint --all --config charts/.ci/ct-config.yaml
+docker run --rm -it -w /repo -v `pwd`:/repo quay.io/helmpack/chart-testing ct list-changed --config charts/.ci/ct-config.yaml
